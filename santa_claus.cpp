a8bbc4192102424416ccb0f7aa1fe6953a12ad02
Paula Ribeiro <paula@useorigin.com>
2022-10-04 22:13:35 -0300

    optimize for time and space

--- santa_claus.cpp
+++ santa_claus.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <set>
-#include <queue>
 
 using namespace std;
 
 #define BOARD_SIDE_SIZE 8
 
-void addEdge(set<pair<int, int> > graph[], int u, int v, int weight);
-int* getMST(set<pair<int, int> > graph[], int size);
+void addEdge(short int** graph, int u, int v, int weight);
+int* getMST(short int** graph, int size);
 int getMinKeyNotInMST(int keys[], int includedInMst[], int size);
-int getDistance(set<pair<int, int> > graph[], int parents[], int size);
-int getEdgeDistance(set<pair<int, int> > graph[], int u, int v);
+int getDistance(short int** graph, int parents[], int size);
+int getEdgeDistance(short int** graph, int u, int v);
 
 int main()
 {
@@ -18,7 +16,14 @@ int main()
     cin >> m >> n;
     while (m > 0)
     {
-        set<pair<int, int> > graph[m];
+        short int** graph = (short int**) malloc(m*sizeof(int *));
+        for (int i=0; i<m; i++) {
+            graph[i] = (short int*) malloc(m*sizeof(int *));
+            for (int j=0; j<m; j++) {
+                graph[i][j] = INT16_MAX;
+            }
+        }
+
         for (int i = 0; i < n; i++)
         {
             int city1, city2, distance;
@@ -30,30 +35,35 @@ int main()
         int* parents = getMST(graph, m);
         int totalDistance = getDistance(graph, parents, m);
         cout << totalDistance << "\n";
+
+        for (int i=0; i<m; i++) {
+            free(graph[i]);
+        }
+        free(graph);
+        free(parents);
+        
         cin >> m >> n;
     }
 
     return 0;
 }
 
-int getDistance(set<pair<int, int> > graph[], int parents[], int size){
+int getDistance(short int** graph, int parents[], int size){
     int totalDistance = 0;
-    for (int i=0; i<size; i++) {
-        if (parents[i] != -1) {
-            totalDistance += getEdgeDistance(graph, i, parents[i]);
-        }
+    for (int i=1; i<size; i++) {
+        totalDistance += graph[i][parents[i]];
     }
     return totalDistance;
 }
 
-int* getMST(set<pair<int, int> > graph[], int size)
+int* getMST(short int** graph, int size)
 {
     int* parents = (int*)malloc(size * sizeof(int));
-    int includedInMst[size];
-    int keys[size];
+    int* includedInMst = (int*)malloc(size * sizeof(int));
+    int* keys = (int*)malloc(size * sizeof(int));
 
     for (int i=0; i<size; i++) {
-        keys[i] = INT32_MAX;
+        keys[i] = INT16_MAX;
         includedInMst[i] = false;
     }
 
@@ -64,19 +74,16 @@ int* getMST(set<pair<int, int> > graph[], int size)
         int minKey = getMinKeyNotInMST(keys, includedInMst, size);
         includedInMst[minKey] = true;
 
-        std::set<pair<int, int> >::iterator it;
-        for (it = graph[minKey].begin(); it != graph[minKey].end(); ++it)
-        {
-            int neighborIndex = (*it).first;
-            int neighborWeight = (*it).second;
-
-            if (!includedInMst[neighborIndex] && neighborWeight < keys[neighborIndex]) {
-                parents[neighborIndex] = minKey;
-                keys[neighborIndex] = neighborWeight;
+        for (int j=0; j<size; j++) {
+            if (!includedInMst[j] && graph[minKey][j] < keys[j]) {
+                parents[j] = minKey;
+                keys[j] = graph[minKey][j];
             }
         }
     }
 
+    free(includedInMst);
+    free(keys);
     return parents;
 }
 
@@ -93,24 +100,8 @@ int getMinKeyNotInMST(int keys[], int includedInMst[], int size) {
     return minIndex;
 }
 
-void addEdge(set<pair<int, int> > graph[], int u, int v, int weight)
+void addEdge(short int** graph, int u, int v, int weight)
 {
-    pair<int, int> vPair = make_pair(v, weight);
-    graph[u].insert(vPair);
-    pair<int, int> uPair = make_pair(u, weight);
-    graph[v].insert(uPair);
-}
-
-int getEdgeDistance(set<pair<int, int> > graph[], int u, int v)
-{   
-    std::set<pair<int, int> >::iterator it;
-    for (it = graph[u].begin(); it != graph[v].end(); ++it)
-    {
-        int neighborIndex = (*it).first;
-        int neighborWeight = (*it).second;
-        if (neighborIndex == v) {
-            return neighborWeight;
-        }
-    }
-    return INT32_MAX;
+    graph[u][v] = weight;
+    graph[v][u] = weight;
 }
